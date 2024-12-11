69a975d57c3367a5d72d59a99648ba3ebc6ffb46
Nurlan Purkhanov <ppurkhanov@gmail.com>
2024-12-11 12:47:22 +0500

    created user model

--- src/database/models.py
+++ src/database/models.py
@@ -1,4 +1,5 @@
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.ext.asyncio import AsyncAttrs
 from sqlalchemy.orm import DeclarativeBase
 
@@ -17,3 +18,23 @@ class Product(BaseModel):
     description: Mapped[str | None]
     category: Mapped[str]
 
+
+class User(BaseModel):
+    __tablename__ = "users"
+
+    email: Mapped[str] = mapped_column(unique=True)
+    name: Mapped[str]
+    password: Mapped[str]
+
+    cart: Mapped['Cart'] = relationship(back_populates='user')
+
+
+class Cart(BaseModel):
+    __tablename__ = "carts"
+
+    user_id: Mapped[int] = mapped_column(ForeignKey('users.id'))
+    user: Mapped['User'] = relationship(back_populates='cart')
+
+    product_id: Mapped[int] = mapped_column(ForeignKey('products.id'))
+
+    quantity: Mapped[int] = mapped_column(default=1)
