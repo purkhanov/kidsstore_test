c468b2ac19b31fcb16ec22b723a372ce366776d4
Nurlan Purkhanov <ppurkhanov@gmail.com>
2024-12-11 15:40:30 +0500

    modified crate user func

--- src/users/repository.py
+++ src/users/repository.py
@@ -1,6 +1,17 @@
+from sqlalchemy.exc import IntegrityError
 from src.database.repositories import SQLAlchemyRepository
 from src.database.models import User
+from src.database.exceptions import AlreadyExists
 
 
 class UserRepository(SQLAlchemyRepository):
     model = User
+
+
+    async def create(self, data):
+        try:
+            return await super().create(data)
+        except IntegrityError as e:
+            raise AlreadyExists()
+        except Exception as e:
+            raise
